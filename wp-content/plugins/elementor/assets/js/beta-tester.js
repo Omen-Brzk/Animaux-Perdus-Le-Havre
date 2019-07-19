b5f82c037c296b53dece89ceb7ed86cdde16ab35
Unknown <maxence.kerfuric@gmail.com>
2019-07-19 17:15:31 +0200

    Update elementor, style news picker darker

--- wp-content/plugins/elementor/assets/js/beta-tester.js
+++ wp-content/plugins/elementor/assets/js/beta-tester.js
@@ -1,4 +1,4 @@
-/*! elementor - v2.6.4 - 17-07-2019 */
+/*! elementor - v2.6.5 - 18-07-2019 */
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
